d9ffad912280b8f2ef64523ab25b93fe41428c66
Netanel David <netanel.d@mce.systems>
2024-02-20 22:26:23 +0200

    convert to mp3

--- src/download.ts
+++ src/download.ts
@@ -1,12 +1,25 @@
+import { path } from "@ffmpeg-installer/ffmpeg";
+import ffmpeg from 'fluent-ffmpeg';
 import { resolve } from "path";
 import { Readable } from "stream";
-import { writeFile, mkdir } from "fs/promises";
+import { mkdir } from "fs/promises";
 import { execAndLog } from "./shared";
 
-export async function download(readableStream: Readable, fileName: string) {
+ffmpeg.setFfmpegPath(path);
+
+export async function download(streamReadable: Readable, fileName: string) {
     const dirPath = resolve(__dirname, "..", "downloads");
     const filePath = resolve(dirPath, fileName);
+
     await mkdir(dirPath, { recursive: true });
-    await execAndLog("Download", () => writeFile(filePath, readableStream));
+    await execAndLog("Convert to mp3 and download", () => {
+        return new Promise((res, rej) => {
+            ffmpeg(streamReadable)
+                .toFormat("mp3")
+                .saveToFile(filePath)
+                .on("end", res)
+                .on("error", rej)
+        })
+    })
     return filePath;
 }
